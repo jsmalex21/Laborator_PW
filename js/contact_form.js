694a8fcd253e8c9968dc1078a371f98dabae88c9
Alexandru Taranu <alexandru.taranu@student.unitbv.ro>
2026-02-25 17:30:38 +0200

    feat: adauga buton back to top

--- js/contact_form.js
+++ js/contact_form.js
@@ -93,4 +93,21 @@ document.addEventListener("DOMContentLoaded", () => {
       this.textContent = `${isHiddenNow ? "▶" : "▼"} ${title}`;
     });
   });
+    // Back to top
+  const backToTopBtn = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTopBtn.classList.remove("hidden");
+    } else {
+      backToTopBtn.classList.add("hidden");
+    }
+  });
+
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 });
